9fd20cfcd11dab443b78af05e33e0ed7ccbc8e71
Kisub Lee <cagongman@gmail.com>
2024-07-21 20:31:19 +0900

    update project filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,14 +73,17 @@ for (let i = 0; i < selectItems.length; i++) {
   });
 }
 
+
 // filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
   for (let i = 0; i < filterItems.length; i++) {
+    const itemCategories = filterItems[i].dataset.category.split(' ');
+
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
+    } else if (itemCategories.includes(selectedValue)) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
